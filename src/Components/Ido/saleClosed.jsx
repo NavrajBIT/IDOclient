58daf60f9dd0d0005423ba81b41f3f8527135e07
NavrajBIT <navraj@beimagine.tech>
2024-03-21 15:53:17 +0530

    sale closed color

--- src/Components/Ido/saleClosed.jsx
+++ src/Components/Ido/saleClosed.jsx
@@ -6,7 +6,7 @@ const SaleClosed = () => {
     <div
       style={{
         position: "fixed",
-        background: "rgba(0,0,0,1)",
+        background: "var(--green-40)",
         top: 0,
         left: 0,
         height: "100vh",
