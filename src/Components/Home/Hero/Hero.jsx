6f04c53dcea586bd9300fab15fb1a2410bbde770
NavrajBIT <navraj@beimagine.tech>
2024-03-25 10:35:58 +0530

    sale progress

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,65 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import './Hero.css';
+import React, { useState, useEffect } from "react";
+import "./Hero.css";
 
 const Hero = () => {
-    const [remainingTime, setRemainingTime] = useState(calculateRemainingTime());
+  //   const [remainingTime, setRemainingTime] = useState(calculateRemainingTime());
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setRemainingTime(calculateRemainingTime());
-        }, 1000);
+  //   useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       setRemainingTime(calculateRemainingTime());
+  //     }, 1000);
 
-        return () => clearInterval(timer);
-    }, []);
+  //     return () => clearInterval(timer);
+  //   }, []);
 
-    function calculateRemainingTime() {
-        const targetDate = new Date('2024-03-25T18:00:00');
-        const now = new Date();
-        const difference = targetDate - now;
+  //   function calculateRemainingTime() {
+  //     const targetDate = new Date("2024-03-25T18:00:00");
+  //     const now = new Date();
+  //     const difference = targetDate - now;
 
-        if (difference <= 0) {
-            return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-        }
+  //     if (difference <= 0) {
+  //       return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  //     }
 
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((difference / 1000 / 60) % 60);
-        const seconds = Math.floor((difference / 1000) % 60);
+  //     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  //     const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  //     const minutes = Math.floor((difference / 1000 / 60) % 60);
+  //     const seconds = Math.floor((difference / 1000) % 60);
 
-        return { days, hours, minutes, seconds };
-    }
+  //     return { days, hours, minutes, seconds };
+  //   }
 
-    return (
-        <>
-            <section className="heroSection">
-                <h1 className="heroMaintext">Welcome to BitBhoomi IDO</h1>
+  return (
+    <>
+      <section className="heroSection">
+        <h1 className="heroMaintext">Welcome to BitBhoomi IDO</h1>
 
-                <h1 className="heroTagline">Where Technology Meets Sustainability</h1>
+        <h1 className="heroTagline">Where Technology Meets Sustainability</h1>
 
-                <div className="timerContainer">
-                    <div className="timerBox">
-                        <h2 className="number">{remainingTime.days}</h2>
-                        <h4 className="unit">Days</h4>
-                    </div>
-                    <h2 className='colon' variant='h2'>:</h2>
-                    <div className="timerBox">
-                        <h2 variant="h4" className="number">{remainingTime.hours}</h2>
-                        <h4 variant="h6" className="unit">Hours</h4>
-                    </div>
-                    <h2 className='colon' variant='h2'>:</h2>
-                    <div className="timerBox">
-                        <h2 variant="h4" className="number">{remainingTime.minutes}</h2>
-                        <h4 variant="h6" className="unit">Mins</h4>
-                    </div>
-                    <h2 className='colon' variant='h2'>:</h2>
-                    <div className="timerBox">
-                        <h2 variant="h4" className="number">{remainingTime.seconds}</h2>
-                        <h4 variant="h6" className="unit">Sec</h4>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+        <div
+          style={{
+            fontSize: "3rem",
+          }}
+          className="heroMaintext"
+        >
+          In Progress
+        </div>
+
+        {/* <div className="timerContainer">
+          <div className="timerBox">
+            <h2 className="number">{remainingTime.days}</h2>
+            <h4 className="unit">Days</h4>
+          </div>
+          <h2 className="colon" variant="h2">
+            :
+          </h2>
+          <div className="timerBox">
+            <h2 variant="h4" className="number">
+              {remainingTime.hours}
+            </h2>
+            <h4 variant="h6" className="unit">
+              Hours
+            </h4>
+          </div>
+          <h2 className="colon" variant="h2">
+            :
+          </h2>
+          <div className="timerBox">
+            <h2 variant="h4" className="number">
+              {remainingTime.minutes}
+            </h2>
+            <h4 variant="h6" className="unit">
+              Mins
+            </h4>
+          </div>
+          <h2 className="colon" variant="h2">
+            :
+          </h2>
+          <div className="timerBox">
+            <h2 variant="h4" className="number">
+              {remainingTime.seconds}
+            </h2>
+            <h4 variant="h6" className="unit">
+              Sec
+            </h4>
+          </div>
+        </div> */}
+      </section>
+    </>
+  );
+};
 
 export default Hero;
